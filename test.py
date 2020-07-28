e75314c195d0e2abbb7a1f6d327b3949905a7013
katerin-b <68336585+katerin-b@users.noreply.github.com>
2020-07-28 19:51:58 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,14 @@
 import unittest
-
-
 def greater_than_five(val: int) -> bool:
     """
     Check if value > 5
     :param val: value to check.
     :return: True if value > 5, else - False
     """
-    return False
-
-
+    return val > 5
 class Test(unittest.TestCase):
     def test(self):
         self.assertTrue(greater_than_five(6))
         self.assertFalse(greater_than_five(4))
+if __name__ == "__main__":
+    unittest.main()
